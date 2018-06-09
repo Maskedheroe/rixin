133972a929c905420036d446cda19da83e2341f4
jcydshanks <jcydshanks@163.com>
2018-06-09 23:43:04 +0800

    add Global.class

---
+++ app/src/main/java/jcydshanks/com/rixin/Global.java
@@ -0,0 +1,30 @@
+package jcydshanks.com.rixin;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by JcyDS on 2018/6/9.
+ * 封装Toast弹窗函数show，调用方法Global.show();
+ */
+
+public class Global {
+
+
+    public static Context gContext;
+
+
+
+    public static void show(String paramString)
+    {
+        try
+        {
+            Toast.makeText(gContext, paramString, Toast.LENGTH_SHORT).show();
+            return;
+        }
+        catch (Exception localException)
+        {
+            localException.printStackTrace();
+        }
+    }
+}
